2cc8227c5059af28a1349629a4289d54c91853d8
Théo Letouzé <theo.letouze44@gmail.com>
2020-12-21 19:01:23 +0100

    ok

--- src/resolver/query.js
+++ src/resolver/query.js
@@ -1,5 +1,19 @@
 const { pool, withClient } = require("../database.js");
 
+/*
+    event state : 
+        0 --> idea
+        1 --> incoming
+        2 --> finish
+*/
 module.exports = {
-  hello: () => "hello world",
+  getDisplayedEvent: withClient(async (_, __, { client }) => {
+    console.log("ok");
+    const { rows: response } = await client.query(
+      'SELECT *, end_date as "endDate" ,start_date as "startDate"  FROM event where state = 2 or state = 1 '
+    );
+    console.log(response);
+    console.log({ ok: response[0].startDate });
+    return response;
+  }),
 };
