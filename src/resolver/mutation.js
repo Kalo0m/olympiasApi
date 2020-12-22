ff48fed18ba577bab3063adb53e12d5d3533eea5
Théo Letouzé <theo.letouze44@gmail.com>
2020-12-22 23:50:45 +0100

    ok

--- src/resolver/mutation.js
+++ src/resolver/mutation.js
@@ -1,5 +1,51 @@
 const { pool, withClient } = require("../database.js");
-
+const { ApolloError } = require("apollo-server");
 module.exports = {
-  hello: () => "hello world",
+  login: withClient(async (_, { login, password }, { client }) => {
+    console.log("login");
+    const {
+      rows: response,
+    } = await client.query(
+      "SELECT name FROM admin WHERE password is NOT NULL AND password = crypt($1,password) AND name = $2;",
+      [password, login]
+    );
+    if (!response || response.length === 0)
+      throw new ApolloError(
+        "login or password incorrect",
+        "USER_DOES_NOT_EXIST"
+      );
+    console.log(response);
+    return response[0];
+  }),
+  saveEvents: withClient(async (_, { events }, { client }) => {
+    console.log("save");
+    const promises = events.map((event) => {
+      return client.query(
+        "update event set description = $1, name = $2, place = $3, state = $4, organizer = $5, start_date = TO_TIMESTAMP($6), end_date = TO_TIMESTAMP($7) where id = $8",
+        [
+          event.description,
+          event.name,
+          event.place,
+          event.state,
+          event.organizer,
+          event.startDate / 1000,
+          event.endDate / 1000,
+          event.id,
+        ]
+      );
+    });
+    let res = false;
+    await Promise.all(promises)
+      .then(() => {
+        res = true;
+      })
+      .catch((err) => {
+        console.log({ err });
+        throw new ApolloError(
+          "La sauvegarde des données a échoué" + err,
+          "SAVE_ERROR"
+        );
+      });
+    return res;
+  }),
 };
--- src/resolver/query.js
+++ src/resolver/query.js
@@ -10,7 +10,7 @@ module.exports = {
   getDisplayedEvent: withClient(async (_, __, { client }) => {
     console.log("ok");
     const { rows: response } = await client.query(
-      'SELECT *, end_date as "endDate" ,start_date as "startDate"  FROM event where state = 2 or state = 1 '
+      'SELECT *, end_date as "endDate" ,start_date as "startDate"  FROM event'
     );
     console.log(response);
     console.log({ ok: response[0].startDate });
